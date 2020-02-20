767a4847fccb66825a68c86205fc72be85e1359f
Henry Kaikko <henry.kaikko@gmail.com>
2020-02-20 07:25:00 +0200

    Add redis settings implementation - include authentication settings in configurations

--- AuthenticationService.Application/Settings/RedisSettings.cs
+++ AuthenticationService.Application/Settings/RedisSettings.cs
@@ -14,7 +14,27 @@ namespace AuthenticationService.Application.Settings
             this.Configuration = configuration;
         }
 
+        public bool AllowAdmin => this.Configuration.GetValue<bool?>("Redis:AllowAdmin") ?? true;
+
+        public bool AbortConnect => this.Configuration.GetValue<bool?>("Redis:AbortConnection") ?? false;
+
         public string AppName => this.Configuration.GetValue<string>("Redis:AppName");
-        public string ConnectionString => this.Configuration.GetValue<string>("Redis:ConnectionString");
+
+        public string ConnectionString
+        {
+            get
+            {
+                var url = $"{this.Host + ":" + this.Port}, abortConnect={this.AbortConnect}, allowAdmin={this.AllowAdmin}";
+
+                if (!String.IsNullOrWhiteSpace(this.Password))
+                    url += $", password={this.Password}";
+
+                return url;
+            }
+        }
+
+        public string Password => this.Configuration.GetValue<string>("Redis:Password");
+        public string Host => this.Configuration.GetValue<string>("Redis:Host");
+        public int Port => this.Configuration.GetValue<int>("Redis:Port");
     }
 }
