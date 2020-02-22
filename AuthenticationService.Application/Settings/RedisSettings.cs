041cdda482ef2b9c97cb0cb46f307e4cc570e5b5
Henry Kaikko <henry.kaikko@gmail.com>
2020-02-22 13:06:55 +0200

    Add expansion for RedisSettings for more properties

--- AuthenticationService.Application/Settings/RedisSettings.cs
+++ AuthenticationService.Application/Settings/RedisSettings.cs
@@ -14,27 +14,30 @@ namespace AuthenticationService.Application.Settings
             this.Configuration = configuration;
         }
 
+        #region Public properties
+
         public bool AllowAdmin => this.Configuration.GetValue<bool?>("Redis:AllowAdmin") ?? true;
-
         public bool AbortConnect => this.Configuration.GetValue<bool?>("Redis:AbortConnection") ?? false;
-
         public string AppName => this.Configuration.GetValue<string>("Redis:AppName");
-
-        public string ConnectionString
-        {
-            get
-            {
-                var url = $"{this.Host + ":" + this.Port}, abortConnect={this.AbortConnect}, allowAdmin={this.AllowAdmin}";
-
-                if (!String.IsNullOrWhiteSpace(this.Password))
-                    url += $", password={this.Password}";
-
-                return url;
-            }
-        }
-
         public string Password => this.Configuration.GetValue<string>("Redis:Password");
         public string Host => this.Configuration.GetValue<string>("Redis:Host");
+        public string DataProtectionKeyName => this.Configuration.GetValue<string>("Redis:DataProtectionKeyName");
         public int Port => this.Configuration.GetValue<int>("Redis:Port");
+
+        #endregion
+
+        #region Public methods
+
+        public string GetConnectionString()
+        {
+            var url = $"{this.Host + ":" + this.Port}, abortConnect={this.AbortConnect}, allowAdmin={this.AllowAdmin}";
+
+            if (!String.IsNullOrWhiteSpace(this.Password))
+                url += $", password={this.Password}";
+
+            return url;
+        }
+
+        #endregion
     }
 }
